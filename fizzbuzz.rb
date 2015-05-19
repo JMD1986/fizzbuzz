2f7c001dfab2b110d232958fed56899201feff53
John Daugherty <johndaugherty1986@gmail.com>
2015-05-19 00:45:44 -0400

    first commit

---
+++ fizzbuzz.rb
@@ -0,0 +1,22 @@
+class Fizzbuzz
+  def initialize
+    @max = max
+  end
+  attr_accessor :max
+  def self.run(num)
+  1.upto(num).each do |x|
+    if x % 3 == 0 && x % 5 == 0
+      puts "fizzbuzz"
+    elsif x % 5 == 0
+      puts "buzz"
+    elsif x % 3 == 0
+      puts "fizz"
+    else
+      puts x
+    end
+  end
+end
+  print "range max number?: "
+  max = gets.chomp
+  run(max.to_i)
+end
